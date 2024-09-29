8fb068de060c6952c732d9e9ae2c9bb89a5478a0
Sergey Ponomarev <stokito@gmail.com>
2024-09-29 19:40:14 +0300

    adhoc.js remove Function.prototype.bind

--- www_files/js/adhoc.js
+++ www_files/js/adhoc.js
@@ -3,29 +3,6 @@
 // This project is MIT licensed.
 // http://git.babelmonkeys.de/?p=adhocweb.git;a=blob_plain;f=js/adhoc.js
 
-/*
- * Implementation of ECMA Script 5 like bind from:
- * https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind
- */
-if (!Function.prototype.bind) {
-  Function.prototype.bind = function (oThis) {
-    if (typeof this !== "function") {
-      /* closest thing possible to the ECMAScript 5 internal IsCallable function */
-      throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-    }
-    var fSlice = Array.prototype.slice,
-        aArgs = fSlice.call(arguments, 1),
-        fToBind = this,
-        fNOP = function () {},
-        fBound = function () {
-          return fToBind.apply(this instanceof fNOP ? this : oThis || window, Args.concat(fSlice.call(arguments)));
-        };
-    fNOP.prototype = this.prototype;
-    fBound.prototype = new fNOP();
-    return fBound;
-  };
-}
-
 Strophe.addNamespace("ADHOC", "http://jabber.org/protocol/commands");
 
 function Adhoc(view, readycb) {
